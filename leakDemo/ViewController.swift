fb4894f56a62562ef85a1f6f9e836b5c1a0bdc74
MonkeySun <492899051@qq.com>
2020-08-15 10:45:24 +0800

    rebase

--- leakDemo/ViewController.swift
+++ leakDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 //master 更新
+        //这是一段测试
     }
 
     override func didReceiveMemoryWarning() {
